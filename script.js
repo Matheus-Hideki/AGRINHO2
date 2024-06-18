72342bcb474f7b10d04643874210f9ff88b3cc18
Matheus Hideki <104567107+Matheus-Hideki@users.noreply.github.com>
2024-06-18 09:42:43 -0300

    ssexo 2

---
+++ script.js
@@ -0,0 +1,8 @@
+var stage0 = 0;
+
+function Proceed()
+{
+    let element1 = document.getElementById("0");
+    element1.setAttribute("hidden", "hidden");
+    //element3.removeAttribute("hidden");
+}
