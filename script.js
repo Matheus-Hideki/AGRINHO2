abc03872ab81ff96eff977cbd3759916adf42028
Matheus Hideki <104567107+Matheus-Hideki@users.noreply.github.com>
2024-06-25 09:54:21 -0300

    IMPERIO PORKY

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var stage0 = 0;
 function Proceed()
 {
     let element1 = document.getElementById("0");
-    element1.setAttribute("hidden", "hidden");
+    element1.style.animationName = "page_flip";
+    element1.style.animationDuration = "3s";
+    element1.style.animationPlayState = "initial";
+    //element1.setAttribute("hidden", "hidden");
     //element3.removeAttribute("hidden");
 }
