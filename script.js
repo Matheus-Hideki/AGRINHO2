20305fb29f153b8e16719191beb1b738e209cfb4
Matheus Hideki <104567107+Matheus-Hideki@users.noreply.github.com>
2024-07-31 07:44:34 -0300

    outrosaowdajji

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ function Proceed()
     //element1.setAttribute("hidden", "hidden");
     //element3.removeAttribute("hidden");
 }
+function start()
+{
+    setInterval(clouLoop,60);
+}
+let additiveCloudX = 0.0;
+let additiveCloudY = 0.0;
+function clouLoop()
+{
+    let cloud = document.getElementById("cloud");
+    additiveCloudX+= 0.1;
+    let additiveX = (Math.sin(additiveCloudX)+10);
+    cloud.style.top = additiveX + "%";
+
+
+    additiveCloudY+= 0.01;
+    let additiveY = (Math.sin(additiveCloudY)+90);
+    cloud.style.left = additiveY + "%";
+}
